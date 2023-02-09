6ca8eda2b2500440ce3ff03f7f1468c20b7d8589
gowjr207@gmail.com <gowjr207@gmail.com>
2023-02-09 23:47:44 +0900

    [FIX] 멤버엔티티 수정

--- member_service/src/main/java/com/luckytree/member_service/member/adapter/out/persistence/MemberEntity.java
+++ member_service/src/main/java/com/luckytree/member_service/member/adapter/out/persistence/MemberEntity.java
@@ -1,17 +1,15 @@
 package com.luckytree.member_service.member.adapter.out.persistence;
 
-import com.luckytree.member_service.member.application.port.in.MemberRequest;
+import com.luckytree.member_service.Photo;
 import com.luckytree.member_service.member.domain.Status;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Table(name = "member")
 @Getter
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 @Entity
 public class MemberEntity {
 
@@ -19,13 +17,13 @@ public class MemberEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 20, unique = true)
+    @Column(length = 20, unique = true, nullable = false)
     private String nickname;
 
-    @Column(length = 50)
+    @Column(length = 50, unique = true, nullable = false)
     private String email;
 
-    @Column(length = 50)
+    @Column(length = 50, nullable = false)
     private String password;
 
     @Column(length = 20, nullable = false)
@@ -33,13 +31,5 @@ public class MemberEntity {
     private Status status;
 
     @Column(length = 50)
-    private String photo;
-
-    public MemberEntity(MemberRequest memberRequest) {
-        this.nickname = memberRequest.getNickname();
-        this.email = memberRequest.getEmail();
-        this.password = memberRequest.getPassword();
-        this.status = Status.NORMAL;
-        this.photo = memberRequest.getPhoto();
-    }
+    private Photo photo;
 }
