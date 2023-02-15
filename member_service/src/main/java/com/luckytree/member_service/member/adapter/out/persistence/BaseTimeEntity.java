73d007ade0eb81ca40071e333bc7a4d303ec4874
InHee <ihcho620@gmail.com>
2023-02-15 20:40:32 +0900

    [FEAT] BaseTimeEntity 생성 및 Entity에 적용.

---
+++ member_service/src/main/java/com/luckytree/member_service/member/adapter/out/persistence/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.luckytree.member_service.member.adapter.out.persistence;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+}
--- member_service/src/main/java/com/luckytree/member_service/member/adapter/out/persistence/MemberEntity.java
+++ member_service/src/main/java/com/luckytree/member_service/member/adapter/out/persistence/MemberEntity.java
@@ -1,17 +1,19 @@
 package com.luckytree.member_service.member.adapter.out.persistence;
 
-import com.luckytree.member_service.Photo;
+import com.luckytree.member_service.member.domain.Photo;
 import com.luckytree.member_service.member.domain.Status;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "member")
 @Getter
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
 @Entity
-public class MemberEntity {
+public class MemberEntity extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +25,7 @@ public class MemberEntity {
     @Column(length = 50, unique = true, nullable = false)
     private String email;
 
-    @Column(length = 50, nullable = false)
+    @Column(length = 50)
     private String password;
 
     @Column(length = 20, nullable = false)
