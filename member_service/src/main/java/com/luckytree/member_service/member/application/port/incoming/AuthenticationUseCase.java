6a6f5a91e6385a4255c63fc05182b4df33b77845
gowjr207@gmail.com <gowjr207@gmail.com>
2023-02-19 00:07:09 +0900

    feat: 인증 유즈케이스 구현

---
+++ member_service/src/main/java/com/luckytree/member_service/member/application/port/incoming/AuthenticationUseCase.java
@@ -0,0 +1,13 @@
+package com.luckytree.member_service.member.application.port.incoming;
+
+import com.luckytree.member_service.member.adapter.data.AuthenticateResult;
+import com.luckytree.member_service.member.adapter.data.SignupDto;
+import com.luckytree.member_service.member.adapter.data.TokenDto;
+import com.luckytree.member_service.member.domain.Authentication;
+
+public interface AuthenticationUseCase {
+
+    AuthenticateResult authenticate(Authentication authentication);
+
+    TokenDto signUp(SignupDto signUpDto);
+}
