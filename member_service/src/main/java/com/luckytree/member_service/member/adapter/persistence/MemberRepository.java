3550466e370a0dba903abe6fc4658c80706c8d58
gowjr207@gmail.com <gowjr207@gmail.com>
2023-02-19 00:06:42 +0900

    feat: 멤버엔티티 생성자 구현 및 jpa exists 메소드 구현

--- member_service/src/main/java/com/luckytree/member_service/member/adapter/persistence/MemberRepository.java
+++ member_service/src/main/java/com/luckytree/member_service/member/adapter/persistence/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.luckytree.member_service.member.adapter.persistence;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -7,4 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
     Optional<MemberEntity> findByNicknameAndEmail(String nickname, String email);
+
+    boolean existsByEmail(String email);
 }
