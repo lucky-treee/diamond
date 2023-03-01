d1333514071680ff583cdc3a29c14c6604011790
jerry8534 <jerryjjinu@gmail.com>
2023-03-01 16:43:47 +0900

    [STYLE] Controller, Port, Adapter, Service, UseCase 통합

--- shop_service/src/main/java/com/luckytree/shop_service/shop/adapter/web/ShopController.java
+++ shop_service/src/main/java/com/luckytree/shop_service/shop/adapter/web/ShopController.java
@@ -27,42 +27,42 @@ public class ShopController {
 
     @Operation(summary = "샵 등록요청 API")
     @PostMapping("/create/request")
-    public ResponseEntity requestShopRegistration(@RequestBody @Valid ShopRequest shopRequest) {
+    public ResponseEntity<Object> requestShopRegistration(@RequestBody @Valid ShopRequest shopRequest) {
         createShopUseCase.requestShopRegistration(shopRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Operation(summary = "특정 카테고리의 샵 전체 검색")
     @GetMapping("/get/category")
-    public ResponseEntity getShopListByCategory(@RequestParam String category) {
+    public ResponseEntity<List<ShopSummary>> getShopListByCategory(@RequestParam String category) {
         List<ShopSummary> shopSummaryList = getShopListUseCase.getShopSummaryByCategory(category);
         return new ResponseEntity<>(shopSummaryList, HttpStatus.OK);
     }
 
     @Operation(summary = "범위 내 샵 전체 검색")
     @GetMapping("/get")
-    public ResponseEntity getShopAll(@RequestParam double maxLat, @RequestParam double minLat, @RequestParam double maxLng, @RequestParam double minLng) {
+    public ResponseEntity<List<ShopSummary>> getShopAll(@RequestParam double maxLat, @RequestParam double minLat, @RequestParam double maxLng, @RequestParam double minLng) {
         List<ShopSummary> shopSummary = getShopListUseCase.getShopAll(maxLat, minLat, maxLng, minLng);
         return new ResponseEntity<>(shopSummary, HttpStatus.OK);
     }
 
     @Operation(summary = "선택된 샵 상세정보 검색")
     @GetMapping("/get/detail")
-    public ResponseEntity getShopDetail(@RequestParam String name, @RequestParam String address) {
+    public ResponseEntity<ShopDetail> getShopDetail(@RequestParam String name, @RequestParam String address) {
         ShopDetail shopDetail = getShopListUseCase.getShopDetail(name, address);
         return new ResponseEntity<>(shopDetail, HttpStatus.OK);
     }
 
     @Operation(summary = "특정 해쉬태크의 샵 상세정보 조회")
     @GetMapping("/get/hashtag")
-    public ResponseEntity getShopSummaryByHashtag(@RequestParam Hashtag hashtag) {
+    public ResponseEntity<List<ShopSummary>> getShopSummaryByHashtag(@RequestParam Hashtag hashtag) {
         List<ShopSummary> shopSummaryList = getShopListUseCase.getShopSummaryByHashtag(hashtag);
         return new ResponseEntity<>(shopSummaryList, HttpStatus.OK);
     }
 
     @Operation(summary = "샵 삭제요청 API")
     @PostMapping("/remove/request")
-    public ResponseEntity removeShopRequest(@RequestBody @Valid RemoveRequestForm removeRequestForm) {
+    public ResponseEntity<Object> removeShopRequest(@RequestBody @Valid RemoveRequestForm removeRequestForm) {
         removeShopUseCase.removeShopRequest(removeRequestForm);
         return new ResponseEntity<>(HttpStatus.OK);
     }
