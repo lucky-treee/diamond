088e7977e32da78d4f271e0c49c91ae3c1c9c1d0
InHee <ihcho620@gmail.com>
2023-03-06 22:39:56 +0900

    [DOS] Swagger 업그레이드

--- shop_service/src/main/java/com/luckytree/shop_service/common/dto/ResultResponse.java
+++ shop_service/src/main/java/com/luckytree/shop_service/common/dto/ResultResponse.java
@@ -1,19 +1,24 @@
 package com.luckytree.shop_service.common.dto;
 
-import jakarta.annotation.Nullable;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Data
 @NoArgsConstructor
+@Schema(description = "샵 api 통신 결과정보")
 public class ResultResponse<T> {
 
+    @Schema(description = "http 상태값")
     private int status;     // http 상태값
+    @Schema(description = "에러 코드")
     private int code;       // 에러 코드
 
+    @Schema(description = "메세지")
     private String message; // 메시지
 
+    @Schema(description = "전송 데이터")
     private T result;       // 전송 데이터
 
     public ResultResponse(T result) {
