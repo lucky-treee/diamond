799abfe3b79b3642abdcb28ab385f6b4f9314060
gowjr207@gmail.com <gowjr207@gmail.com>
2023-03-11 00:31:31 +0900

    refactor: 인증컨트롤러 응답객체 생성방식 변경

--- member_service/src/main/java/com/luckytree/member_service/member/adapter/web/AuthController.java
+++ member_service/src/main/java/com/luckytree/member_service/member/adapter/web/AuthController.java
@@ -1,12 +1,11 @@
 package com.luckytree.member_service.member.adapter.web;
 
+import com.luckytree.member_service.member.adapter.data.LoginDto;
 import com.luckytree.member_service.member.adapter.data.SignupDto;
 import com.luckytree.member_service.member.adapter.data.TokenDto;
 import com.luckytree.member_service.member.application.port.incoming.AuthenticationUseCase;
-import com.luckytree.member_service.member.adapter.data.LoginDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,16 +19,16 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<Object> login(@Valid @RequestBody LoginDto loginDto) {
         authenticationUseCase.login(loginDto);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/signup")
     public ResponseEntity<TokenDto> signup(@Valid @RequestBody SignupDto signupDto) {
-        return new ResponseEntity<>(authenticationUseCase.signup(signupDto), HttpStatus.OK);
+        return ResponseEntity.ok(authenticationUseCase.signup(signupDto));
     }
 
     @GetMapping("/login/kakao")
     public ResponseEntity<TokenDto> loginByKakao(@RequestParam(value = "code") String code) {
-        return new ResponseEntity<>(authenticationUseCase.login(code), HttpStatus.OK);
+        return ResponseEntity.ok(authenticationUseCase.login(code));
     }
 }
