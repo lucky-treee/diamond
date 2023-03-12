371315ba78e5b232d263329e2434909e7088776d
gowjr207@gmail.com <gowjr207@gmail.com>
2023-03-12 20:20:06 +0900

    refactor: 인증 리팩토링

--- member_service/src/main/java/com/luckytree/member_service/member/adapter/persistence/AuthenticationAdapter.java
+++ member_service/src/main/java/com/luckytree/member_service/member/adapter/persistence/AuthenticationAdapter.java
@@ -1,9 +1,15 @@
 package com.luckytree.member_service.member.adapter.persistence;
 
 import com.luckytree.member_service.common.advice.NotFoundException;
+import com.luckytree.member_service.member.adapter.data.KakaoTokenRequest;
+import com.luckytree.member_service.member.adapter.data.KakaoTokenResponse;
+import com.luckytree.member_service.member.adapter.data.KakaoUserInfo;
+import com.luckytree.member_service.member.adapter.feign.KakaoTokenFeignClient;
+import com.luckytree.member_service.member.adapter.feign.KakaoUserInfoFeignClient;
 import com.luckytree.member_service.member.application.port.outgoing.AuthenticationPort;
 import com.luckytree.member_service.member.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
@@ -11,6 +17,17 @@ import org.springframework.stereotype.Repository;
 public class AuthenticationAdapter implements AuthenticationPort {
 
     private final MemberRepository memberRepository;
+    private final KakaoTokenFeignClient kakaoTokenFeignClient;
+    private final KakaoUserInfoFeignClient kakaoUserInfoFeignClient;
+
+    @Value("${oauth2.kakao.clientId}")
+    String clientId;
+
+    @Value("${oauth2.kakao.secretKey}")
+    String clientSecret;
+
+    @Value("${oauth2.kakao.redirectUri}")
+    String redirectUri;
 
     @Override
     public long saveMember(Member member) {
@@ -20,5 +37,17 @@ public class AuthenticationAdapter implements AuthenticationPort {
     @Override
     public long findMemberIdByEmail(String email) {
         return memberRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("해당 이메일에 일치하는 회원이 없습니다.")).getId();
+    }
+
+    @Override
+    public String getUserKakaoAccessToken(String code) {
+        KakaoTokenResponse kakaoTokenResponse = kakaoTokenFeignClient.getToken(new KakaoTokenRequest(code, clientId, clientSecret, redirectUri).toString());
+        return kakaoTokenResponse.getAccessToken();
+    }
+
+    @Override
+    public String getUserKakaoEmail(String accessToken) {
+        KakaoUserInfo kakaoUserInfo = kakaoUserInfoFeignClient.getUser(accessToken);
+        return kakaoUserInfo.getEmail();
     }
 }
