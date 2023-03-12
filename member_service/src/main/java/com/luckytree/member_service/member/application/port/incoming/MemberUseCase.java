87d0b5592b85c12f71adf4a164db46f620c5bbe0
InHee <ihcho620@gmail.com>
2023-03-12 15:59:57 +0900

    [STYLE] deleteMemberRequest() 입력 값 수정.

--- member_service/src/main/java/com/luckytree/member_service/member/application/port/incoming/MemberUseCase.java
+++ member_service/src/main/java/com/luckytree/member_service/member/application/port/incoming/MemberUseCase.java
@@ -2,11 +2,10 @@ package com.luckytree.member_service.member.application.port.incoming;
 
 import com.luckytree.member_service.member.domain.MemberProfile;
 import com.luckytree.member_service.member.domain.Photo;
-import com.luckytree.member_service.member.domain.Status;
 
 public interface MemberUseCase {
 
     MemberProfile getMemberProfile(String nickname);
     void updateMemberRequest(String email, String nickname, Photo photo);
-    void deleteMemberRequest(String email, Status status);
+    void deleteMemberRequest(String email);
 }
