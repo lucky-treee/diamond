7fa08870c442db64023e125742862ae54ccadb81
gowjr207@gmail.com <gowjr207@gmail.com>
2023-03-12 22:13:41 +0900

    feat: 멤버 스웨거 설정 구현

---
+++ member_service/src/main/java/com/luckytree/member_service/common/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.luckytree.member_service.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Member API")
+                .version("v1")
+                .description("회원");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
