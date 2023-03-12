da264ec8371de7ed9c43c1a69b9807f4b9b96d50
gowjr207@gmail.com <gowjr207@gmail.com>
2023-03-12 17:27:19 +0900

    refactor: 토큰 포트 분리

--- member_service/src/main/java/com/luckytree/member_service/member/application/port/incoming/AuthenticationUseCase.java
+++ member_service/src/main/java/com/luckytree/member_service/member/application/port/incoming/AuthenticationUseCase.java
@@ -1,7 +1,7 @@
 package com.luckytree.member_service.member.application.port.incoming;
 
 import com.luckytree.member_service.member.adapter.data.SignupDto;
-import com.luckytree.member_service.member.adapter.data.TokenDto;
+import com.luckytree.member_service.member.adapter.data.Tokens;
 import com.luckytree.member_service.member.adapter.data.LoginDto;
 
 /**
@@ -29,7 +29,7 @@ public interface AuthenticationUseCase {
 
     void login(LoginDto loginDto);
 
-    TokenDto signup(SignupDto signUpDto);
+    Tokens signup(SignupDto signUpDto);
 
-    TokenDto login(String code);
+    Tokens login(String code);
 }
---
+++ member_service/src/main/java/com/luckytree/member_service/member/application/port/outgoing/TokenPort.java
@@ -0,0 +1,10 @@
+package com.luckytree.member_service.member.application.port.outgoing;
+
+public interface TokenPort {
+
+    void validateToken(String token);
+
+    String createAccessToken(long userId);
+
+    String createRefreshToken();
+}
