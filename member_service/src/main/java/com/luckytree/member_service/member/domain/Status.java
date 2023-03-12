637f68de1664377949748235a480c61842ecfcd6
InHee <ihcho620@gmail.com>
2023-03-13 01:15:58 +0900

    [FEAT] Status 내에 검증 로직 구현.

--- member_service/src/main/java/com/luckytree/member_service/member/domain/Status.java
+++ member_service/src/main/java/com/luckytree/member_service/member/domain/Status.java
@@ -1,5 +1,18 @@
 package com.luckytree.member_service.member.domain;
 
+import com.luckytree.member_service.common.advice.BadRequestException;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public enum Status {
-    NORMAL, DORMANT, LEAVE
+    NORMAL, DORMANT, LEAVE;
+
+    public Status isMemberStatus(Status status) {
+        if (status == LEAVE) {
+            throw new BadRequestException("이미 탈퇴 처리된 회원입니다.");
+        }
+        return LEAVE;
+    }
 }
