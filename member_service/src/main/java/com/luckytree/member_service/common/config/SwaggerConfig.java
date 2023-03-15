0ae1db9cb668cd665a0985d2320b1d2b23801f47
tutrlehopper <ihcho620@gmail.com>
2023-03-15 22:42:29 +0900

    [FEAT] reponse 응답을 SwaggerConfig에 globalResponseMesseage로 응답하게 구현.(Member)

--- member_service/src/main/java/com/luckytree/member_service/common/config/SwaggerConfig.java
+++ member_service/src/main/java/com/luckytree/member_service/common/config/SwaggerConfig.java
@@ -3,8 +3,13 @@ package com.luckytree.member_service.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,5 +23,26 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .components(new Components())
                 .info(info);
+    }
+
+    @Bean
+    public OpenApiCustomizer customerGolbalHeaderOpenApiCustomiser() {
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+                ApiResponses apiResponses = operation.getResponses();
+                ApiResponse ok = new ApiResponse().description("ok")
+                        .content(new Content()
+                                .addMediaType(MediaType.APPLICATION_JSON_VALUE, new io.swagger.v3.oas.models.media.MediaType()));
+                apiResponses.addApiResponse("200", ok);
+                ApiResponse badRequest = new ApiResponse().description("Bad Request")
+                        .content(new Content()
+                                .addMediaType(MediaType.APPLICATION_JSON_VALUE, new io.swagger.v3.oas.models.media.MediaType()));
+                apiResponses.addApiResponse("400", badRequest);
+                ApiResponse serverError = new ApiResponse().description("Server Error")
+                        .content(new Content()
+                                .addMediaType(MediaType.APPLICATION_JSON_VALUE, new io.swagger.v3.oas.models.media.MediaType()));
+                apiResponses.addApiResponse("500", serverError);
+            }));
+        };
     }
 }
