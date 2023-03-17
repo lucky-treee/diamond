4ab4104e435a5b55fdcde03e2bf786451db3e2e5
gowjr207@gmail.com <gowjr207@gmail.com>
2023-03-17 21:58:33 +0900

    refactor: 페인클라이언트 기본 적용

--- shop_service/src/main/java/com/luckytree/shop_service/shop/adapter/feign/MemberFeignClient.java
+++ shop_service/src/main/java/com/luckytree/shop_service/shop/adapter/feign/MemberFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "member-service", configuration = FeignClientConfig.class)
-public interface FeignClients {
+public interface MemberFeignClient {
 
     @GetMapping("member/internal-test")
     String call(@RequestParam("name")String name);
