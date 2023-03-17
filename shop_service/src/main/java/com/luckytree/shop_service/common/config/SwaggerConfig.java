a7133a3139038742d9696420d8be6d789e33b22e
gowjr207@gmail.com <gowjr207@gmail.com>
2023-03-17 21:47:40 +0900

    refactor: 주석제거, 응답클래스 제거

--- shop_service/src/main/java/com/luckytree/shop_service/common/config/SwaggerConfig.java
+++ shop_service/src/main/java/com/luckytree/shop_service/common/config/SwaggerConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-// SwaggerConfig.java
 @Configuration
 public class SwaggerConfig{
 
