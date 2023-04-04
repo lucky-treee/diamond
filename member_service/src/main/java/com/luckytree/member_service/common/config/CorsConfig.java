976d860bf48db00753b1f48cab2d5e2fde9d0421
장준영 <gowjr207@gmail.com>
2023-04-04 22:27:14 +0900

    TEST: CORS 오리진 임시로 전체 오픈

--- member_service/src/main/java/com/luckytree/member_service/common/config/CorsConfig.java
+++ member_service/src/main/java/com/luckytree/member_service/common/config/CorsConfig.java
@@ -11,16 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://localhost:5173",
-                        "https://127.0.0.1:5173",
-                        "http://localhost:5173",
-                        "http://127.0.0.1:5173",
-                        "https://local.c0dewave.com:5173",
-                        "https://dev-poom.c0dewave.com",
-                        "https://stg-poom.c0dewave.com",
-                        "https://poom.c0dewave.com",
-                        "http://local-poom.c0dewave.com:5173",
-                        "https://local-poom.c0dewave.com:5173")
+                        "*")
                 .allowedMethods(
                         "GET",
                         "POST",
