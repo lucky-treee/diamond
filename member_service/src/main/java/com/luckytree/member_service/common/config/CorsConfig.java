8ee9a8d79d4eed2c8ce222e99b4479c82f261fd5
장준영 <gowjr207@gmail.com>
2023-04-04 23:05:51 +0900

    TEST: CORS allow credential true

--- member_service/src/main/java/com/luckytree/member_service/common/config/CorsConfig.java
+++ member_service/src/main/java/com/luckytree/member_service/common/config/CorsConfig.java
@@ -11,7 +11,16 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "*")
+                        "https://localhost:5173",
+                        "https://127.0.0.1:5173",
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173",
+                        "https://local.c0dewave.com:5173",
+                        "https://dev-poom.c0dewave.com",
+                        "https://stg-poom.c0dewave.com",
+                        "https://poom.c0dewave.com",
+                        "http://local-poom.c0dewave.com:5173",
+                        "https://local-poom.c0dewave.com:5173")
                 .allowedMethods(
                         "GET",
                         "POST",
@@ -20,6 +29,7 @@ public class CorsConfig implements WebMvcConfigurer {
                         "DELETE"
                 )
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
