34fd410af05100c4e7a6c0a4a73d57135bf6e620
jerry8534 <jerryjjinu@gmail.com>
2023-04-05 23:15:43 +0900

    Revert "style : BookmarkController deleteBookmark 파라미터 어노테이션 변경"
    This reverts commit 940a62fca9cf2c36b3a801a590f1febfbe3bacff.

--- shop_service/src/main/java/com/luckytree/shop_service/shop/adapter/web/BookmarkController.java
+++ shop_service/src/main/java/com/luckytree/shop_service/shop/adapter/web/BookmarkController.java
@@ -24,8 +24,8 @@ public class BookmarkController {
     }
 
     @Operation(summary = "샵 북마크 해제")
-    @DeleteMapping("/bookmark")
-    ResponseEntity<Object> deleteBookmark(@LoginMemberId long memberId, @RequestParam(name = "shopId") long shopId) {
+    @DeleteMapping("/bookmark/{memberId}")
+    ResponseEntity<Object> deleteBookmark(@PathVariable@LoginMemberId long memberId, @RequestParam(name = "shopId") long shopId) {
         shopUseCase.deleteBookmark(memberId, shopId);
         return ResponseEntity.noContent().build();
     }
