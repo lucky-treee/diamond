860ce0a2822472300e916ce0a2ced8ec0349f180
InHee <ihcho620@gmail.com>
2023-04-05 23:54:36 +0900

    [CHORE] 내부 api path internal keyword 제거로 인한 코드 수정.

--- member_service/src/main/java/com/luckytree/member_service/member/adapter/web/internal/internalBookmarkController.java
+++ member_service/src/main/java/com/luckytree/member_service/member/adapter/web/internal/internalBookmarkController.java
@@ -1,17 +1,14 @@
 package com.luckytree.member_service.member.adapter.web.internal;
 
-import com.luckytree.member_service.common.annotation.LoginMemberId;
 import com.luckytree.member_service.member.adapter.data.CreateBookmarkRequest;
 import com.luckytree.member_service.member.application.port.incoming.BookmarkUseCase;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Internal API", description = "즐겨찾기")
 @RestController
-@RequestMapping("/v1/internal/bookmarks")
+@RequestMapping("/v1/bookmarks")
 @RequiredArgsConstructor
 public class internalBookmarkController {
 
@@ -22,8 +19,8 @@ public class internalBookmarkController {
         bookmarkUseCase.create(createBookmarkRequest);
     }
 
-    @DeleteMapping
-    public void deleteBookmark(@LoginMemberId long memberId, @RequestParam(name = "shopId") long shopId) {
+    @DeleteMapping("/{memberId}")
+    public void deleteBookmark(@PathVariable long memberId, @RequestParam(name = "shopId") long shopId) {
         bookmarkUseCase.delete(memberId, shopId);
     }
 } 
