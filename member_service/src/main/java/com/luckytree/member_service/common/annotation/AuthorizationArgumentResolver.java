9ae22dc61d5cb567d731096080d9a787837c99e5
장준영 <gowjr207@gmail.com>
2023-04-10 23:42:02 +0900

    refactor: 토큰 파싱 지점 변경

--- member_service/src/main/java/com/luckytree/member_service/common/annotation/AuthorizationArgumentResolver.java
+++ member_service/src/main/java/com/luckytree/member_service/common/annotation/AuthorizationArgumentResolver.java
@@ -1,10 +1,14 @@
 package com.luckytree.member_service.common.annotation;
 
+import com.luckytree.member_service.common.advice.BadRequestException;
 import com.luckytree.member_service.common.advice.NotFoundException;
+import com.luckytree.member_service.common.advice.UnAuthorizedException;
 import com.luckytree.member_service.common.jwt.TokenProvider;
+import io.jsonwebtoken.*;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -19,8 +23,8 @@ import java.util.Objects;
 @Component
 public class AuthorizationArgumentResolver implements HandlerMethodArgumentResolver {
 
-    @Autowired
-    private TokenProvider tokenProvider;
+    @Value("${jwt.secret}")
+    String secret;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -40,6 +44,29 @@ public class AuthorizationArgumentResolver implements HandlerMethodArgumentResol
         }
 
         log.info("member accessToken :: {}", accessToken);
-        return tokenProvider.getMemberIdByDecoding(accessToken);
+        return getMemberIdByDecoding(accessToken);
+    }
+
+    private Long getMemberIdByDecoding(String token) {
+        validateToken(token);
+        String memberId = Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token).getBody().getSubject();
+        log.info("member id :: {}", memberId);
+        return Long.parseLong(memberId);
+    }
+
+    private void validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token);
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            throw new BadRequestException("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            throw new UnAuthorizedException("만료된 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            throw new BadRequestException("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException("JWT 토큰이 잘못되었습니다.");
+        } catch (MissingClaimException e) {
+            throw new BadRequestException("JWT 토큰의 페이로드가 없습니다.");
+        }
     }
 }
