fbd70f103749ec7cb313b7b9485abe3623006525
장준영 <gowjr207@gmail.com>
2023-05-21 22:52:56 +0900

    style: change class name

--- member/src/main/java/com/luckytree/member/member/adapter/feign/ShopFeignClient.java
+++ member/src/main/java/com/luckytree/member/member/adapter/feign/ShopFeignClient.java
@@ -1,13 +1,13 @@
 package com.luckytree.member.member.adapter.feign;
 
-import com.luckytree.member.common.config.FeignClientConfig;
+import com.luckytree.member.common.config.InternalFeignConfig;
 import com.luckytree.member.member.adapter.data.BookmarksResponse;
 import com.luckytree.member.member.adapter.data.FindBookmarkedShops;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "shop-service", configuration = {FeignClientConfig.class})
+@FeignClient(name = "shop-service", configuration = {InternalFeignConfig.class})
 public interface ShopFeignClient {
 
     @PostMapping("/v1/bookmarks/shops")
