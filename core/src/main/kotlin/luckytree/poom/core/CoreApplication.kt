54ae2a2aa24d0fc19f86329713b0d2d938096f6b
SheepEatLion <gowjr207@gmail.com>
2023-05-21 17:13:35 +0900

    feat: application config in core

--- core/src/main/kotlin/luckytree/poom/core/CoreApplication.kt
+++ core/src/main/kotlin/luckytree/poom/core/CoreApplication.kt
@@ -1,8 +1,10 @@
 package luckytree.poom.core
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class CoreApplication
 
