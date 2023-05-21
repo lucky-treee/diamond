07d974a8a580b3864c7473c833c4953e3645b7d5
SheepEatLion <gowjr207@gmail.com>
2023-05-21 16:42:50 +0900

    feat: component scan

--- auth/src/main/kotlin/com/example/auth/AuthApplication.kt
+++ auth/src/main/kotlin/com/example/auth/AuthApplication.kt
@@ -1,11 +1,13 @@
 package com.example.auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 
+@ConfigurationPropertiesScan
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["com.example.auth", "luckytree.poom.core"])
 class AuthApplication
 
 fun main(args: Array<String>) {
