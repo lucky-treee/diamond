d038e7902742f5d13d322168a5b507b31bd65572
SheepEatLion <gowjr207@gmail.com>
2023-05-21 13:12:36 +0900

    chore: remove no-use import

--- core/src/main/kotlin/luckytree/poom/core/config/SecurityConfig.kt
+++ core/src/main/kotlin/luckytree/poom/core/config/SecurityConfig.kt
@@ -5,23 +5,17 @@ import feign.Request.HttpMethod.*
 import luckytree.poom.core.filter.JwtFilter
 import luckytree.poom.core.handler.JwtAccessDeniedHandler
 import luckytree.poom.core.handler.JwtAuthenticationEntryPoint
-import luckytree.poom.core.service.TokenProvider
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
 
 @EnableWebSecurity
 @Configuration
