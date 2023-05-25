0ab856ec15ae2ac94fc4051cfccb70f8c2c715b6
jerry8534 <jerryjjinu@gmail.com>
2023-05-25 22:34:42 +0900

    refactor : Swagger multipart/form-data 적용

--- shop/src/main/java/com/luckytree/shop/shop/adapter/jpa/review/ReviewPhotoEntity.java
+++ shop/src/main/java/com/luckytree/shop/shop/adapter/jpa/review/ReviewPhotoEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Entity
 @AllArgsConstructor
 @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class ReviewPhotoEntity {
 
     @Id
