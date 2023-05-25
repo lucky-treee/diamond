87b1265f0507bd7e2146b0e712736b965d119026
장준영 <gowjr207@gmail.com>
2023-05-26 00:33:12 +0900

    refactor: add request param annotation

--- auth/src/main/kotlin/com/example/auth/client/feign/member/MemberFeignClient.kt
+++ auth/src/main/kotlin/com/example/auth/client/feign/member/MemberFeignClient.kt
@@ -5,10 +5,10 @@ import com.example.auth.client.feign.member.data.MemberFeignResponse
 import com.example.auth.config.InternalFeignConfig
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(value = "member", url = "\${feign.member}", configuration = [InternalFeignConfig::class])
 interface MemberFeignClient {
     @GetMapping("v1/members")
-    fun findByIdAndEmail(memberFeignRequest: MemberFeignRequest): MemberFeignResponse
+    fun findByIdAndEmail(@RequestParam memberFeignRequest: MemberFeignRequest): MemberFeignResponse
 }
